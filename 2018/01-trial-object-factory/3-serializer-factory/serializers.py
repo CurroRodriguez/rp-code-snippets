4e6c1267960071808f48ff6c42718fb2dd3c87b1
Isaac Rodriguez <isaac_rodriguez@live.com>
2018-12-08 16:43:36 -0500

    Added additional examples

---
+++ 2018/01-trial-object-factory/3-serializer-factory/serializers.py
@@ -0,0 +1,34 @@
+import json
+import xml.etree.ElementTree as et 
+
+class JsonSerializer:
+    def __init__(self):
+        self._current_object = None
+
+    def start_object(self, object_name, object_id):
+        self._current_object = {
+            'id': object_id
+        }
+
+    def add_property(self, name, value):
+        self._current_object[name] = value
+
+    def to_str(self):
+        return json.dumps(self._current_object)
+
+
+class XmlSerializer:
+    def __init__(self):
+        self._element = None
+
+    def start_object(self, object_name, object_id):
+        self._element = et.Element(object_name, attrib={'id': object_id})
+
+    def add_property(self, name, value):
+        prop = et.SubElement(self._element, name)
+        prop.text = value
+
+    def to_str(self):
+        et.tostring(self._element, encoding='unicode')
+
+
